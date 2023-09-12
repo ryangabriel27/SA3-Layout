79d3cdded2e528efdc4384fad3198f85c87578eb
ryangabriel27 <ryanjulio56@gmail.com>
2023-09-12 16:15:23 -0300

    layout atualizadinho pae

--- src/Calculadora1.java
+++ src/Calculadora1.java
@@ -6,35 +6,49 @@ public class Calculadora1 extends JPanel {
         super();
         // IMC
         // Peso(KG)/Altura²(m²)
+
+        // componentes
+        JPanel tudo = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        tudo.setSize(300, 300);
+        this.add(tudo);
         JPanel painel1 = new JPanel(new BorderLayout());
-        this.add(painel1);
-        painel1.add(new JLabel("Calculadora 1"), BorderLayout.NORTH);
-        JPanel campos = new JPanel(new GridLayout(1, 4));
-        painel1.add(campos, BorderLayout.CENTER);
-        campos.add(new JLabel("Insira seu peso:"));
-        JTextField campoPeso = new JTextField(8);
+        tudo.add(painel1);
+        JPanel cabecalho = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        cabecalho.add(new JLabel("Calculadora de IMC"));
+        painel1.add(cabecalho, BorderLayout.NORTH);
+        JPanel calculadora = new JPanel(new BorderLayout());
+        painel1.add(calculadora,BorderLayout.CENTER);
+        JPanel campos = new JPanel(new GridLayout(2, 2,0,3));
+        calculadora.add(campos, BorderLayout.NORTH);
+        campos.add(new JLabel("Insira seu peso (Kg):"));
+        JTextField campoPeso = new JTextField();
         campos.add(campoPeso);
-        campos.add(new JLabel("Insira sua altura:"));
-        JTextField campoAltura = new JTextField(8);
+        campos.add(new JLabel("Insira sua altura (m):"));
+        JTextField campoAltura = new JTextField();
         campos.add(campoAltura);
         JButton calcular = new JButton("Calcular");
-        painel1.add(calcular, BorderLayout.AFTER_LAST_LINE);
-        JPanel resultado = new JPanel(new GridLayout(1,1));
+        calculadora.add(calcular, BorderLayout.CENTER);
         JTextField display = new JTextField();
-        display.setEditable(false);;
-        painel1.add(resultado,BorderLayout.AFTER_LINE_ENDS);
-        resultado.setVisible(true);
+        display.setEditable(false);
+        calculadora.add(display,BorderLayout.SOUTH);
+
+        // ao clicar no botão 'calcular'
         calcular.addActionListener(e -> {
             double imc = 0;
-            if (campoPeso.getText() != "" && campoAltura.getText() != "") {
+            try {
+                if (campoPeso.getText() != "" && campoAltura.getText() != "") {
                 double peso = Double.parseDouble(campoPeso.getText());
                 double altura = Double.parseDouble(campoAltura.getText());
 
                 imc = peso / (altura * altura);
+                display.setText("Seu IMC é de: "+String.valueOf(imc));
                 
+                }
+            } catch (Exception i){
+                display.setText("Preencha os campos corretamente , por favor");
             }
-            display.setText(String.valueOf(imc));
-            resultado.add(display);
+            
+            
             campoPeso.setText("");
             campoAltura.setText("");
         });
--- src/Calculadora2.java
+++ src/Calculadora2.java
@@ -1,9 +1,54 @@
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.text.DecimalFormat;
 
 public class Calculadora2 extends JPanel {
     public Calculadora2() {
         super();
-        this.add(new JLabel("Calculadora 2"));
+        JPanel tudo = new JPanel(new BorderLayout());
+        this.add(tudo);
+        JPanel cabecalho = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        cabecalho.add(new JLabel("Calculadora De Desconto"));
+        tudo.add(cabecalho, BorderLayout.NORTH);
+        JPanel panel = new JPanel(new GridLayout(4, 2));
+        tudo.add(panel, BorderLayout.CENTER);
+
+        JLabel originalPriceLabel = new JLabel("Preço Original:");
+        panel.add(originalPriceLabel);
+
+        JTextField originalPriceField = new JTextField();
+        originalPriceField.setSize(200, 100);
+        panel.add(originalPriceField);
+
+       JLabel discountRateLabel = new JLabel("Taxa de Desconto (%):");
+        panel.add(discountRateLabel);
+
+        JTextField discountRateField = new JTextField();
+        panel.add(discountRateField);
+        
+        originalPriceField.setSize(200, 100);
+        JButton calculateButton = new JButton("Calcular Desconto");
+        panel.add(calculateButton);
+        JLabel resultLabel = new JLabel();
+        panel.add(resultLabel);
+
+        calculateButton.addActionListener(e -> {
+            try {
+                double originalPrice = Double.parseDouble(originalPriceField.getText());
+                double discountRate = Double.parseDouble(discountRateField.getText());
+
+                if (discountRate < 0 || discountRate > 100) {
+                    resultLabel.setText("A taxa de desconto deve estar entre 0 e 100.");
+                } else {
+                    double discountAmount = (originalPrice * discountRate) / 100;
+                    double finalPrice = originalPrice - discountAmount;
+
+                    DecimalFormat df = new DecimalFormat("#.##");
+                    resultLabel.setText("Preço com Desconto: R$ " + df.format(finalPrice));
+                }
+            } catch (NumberFormatException ex) {
+                resultLabel.setText("Valores inválidos. Certifique-se de inserir números válidos.");
+            }
+        });
     }
 }
